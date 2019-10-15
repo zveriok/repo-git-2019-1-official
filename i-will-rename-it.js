a020aa094d357feee537c2c8a2171cfb55ad8a1b
baumvad-global <baumvad-my-global@yandex.ru>
2019-10-15 17:11:06 -0500

    Commit of Second programmer to Vasia branch

--- i-will-rename-it.js
+++ i-will-rename-it.js
@@ -1,13 +1,17 @@
 // some content from 1st proger
+//Ver 2.0 от второго программиста
+
 
 const a =6
 
- if(a>2) {
+ //Я (второй прогер меняю здесь условие)
+ if(a>10) {
 
      console.log("a - is a big number");
  } else {
 
      console.log("a<2");
+ //    а тут я забыл поменять
 
 
  }
